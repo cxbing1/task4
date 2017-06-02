e5ee9bbf962d3c0e11b0a0c521162c4b3990b581
cxbing1 <983121062@qq.com>
2017-06-02 22:56:25 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -1,2 +1,13 @@
 // Write your code here
-module.exports = Person
+module.exports =function Person(name,age)
+{
+
+this.name=name;
+this.age=age;
+this.introduce=function()
+{
+     return "My name is"+name+". I am "+age+" years old.";
+};
+
+}
+
