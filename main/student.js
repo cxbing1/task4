18664673cf286396f6206a87504766865862395d
cxbing1 <983121062@qq.com>
2017-06-02 22:54:16 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -1,3 +1,11 @@
 let Person = require('./person')
 // Write your code here
-module.exports = Student
+module.exports = function Student(name,age,xlass)
+{
+  Person.call(this,name,age);
+ this.introduce=function()
+{
+     return "My name is "+name+". I am "+age+" years old. I am a Student. I am at Class "+xlass+".";
+};
+}
+
